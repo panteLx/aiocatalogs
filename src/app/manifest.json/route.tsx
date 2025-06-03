d5ce27a0af377aee3b3ba63989163d4c2e0d4b43
panteLx <info@pantelx.com>
2025-06-03 23:19:02 +0200

    chore: update logo URL in user catalog manifests for consistency

--- src/app/manifest.json/route.tsx
+++ src/app/manifest.json/route.tsx
@@ -37,7 +37,7 @@ export async function GET(): Promise<NextResponse> {
     version: packageJson.version,
     name: `AIOCatalogs`,
     description: packageJson.description,
-    logo: "https://i.imgur.com/fRPYeIV.png",
+    logo: "https://cdn.ssx.si/u/LSXKCT.png",
     background: "https://i.imgur.com/QPPXf5T.jpeg",
     resources: ["catalog"],
     types: ["movie", "series"],
